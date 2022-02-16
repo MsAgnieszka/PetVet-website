8ea38311215d6b65d58cb0cf2604668195fc033b
Agnieszka Uzarek <agnieszka.czerebak@gmail.com>
2022-02-16 15:17:34 +0100

    edit index.html, add RODO window and style changes in scss

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const nav = document.querySelector('.navbar')
-    const allNavItems = document.querySelectorAll('.nav-link')
-    const navList = document.querySelector('.navbar-collapse')
-    const footerYear = document.querySelector('.footer-year')
+	const nav = document.querySelector('.navbar');
+	const allNavItems = document.querySelectorAll('.nav-link');
+	const navList = document.querySelector('.navbar-collapse');
+	const footerYear = document.querySelector('.footer-year');
 
-    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
-    
-    const handleCurrentYear = () => {
-        const year = (new Date).getFullYear();
-        footerYear.innerText = year;
-    }
-    handleCurrentYear();
-})
+	allNavItems.forEach((item) =>
+		item.addEventListener('click', () => navList.classList.remove('show'))
+	);
+
+	const handleCurrentYear = () => {
+		const year = new Date().getFullYear();
+		footerYear.innerText = year;
+	};
+	handleCurrentYear();
+});
+
+const rodoDescription = document.querySelector('.rodo-description');
+const rodoInfoBox = document.querySelector('.rodo-infobox');
+
+
+
+
+const showRodo = () => {
+	rodoInfoBox.classList.add('show-rodo-infobox');
+};
+
+const hideRodo = () => {
+	rodoInfoBox.classList.remove('show-rodo-infobox');
+};
+
+rodoDescription.addEventListener('mouseenter', showRodo);
+rodoDescription.addEventListener('mouseleave', hideRodo);
